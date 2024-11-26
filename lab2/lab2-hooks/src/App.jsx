35bb6d45001f9211f262b12641ca18c8deb4175f
Корецький Юрій <ipz221_kyui@student.ztu.edu.ua>
2024-11-26 15:53:41 +0200

    modified lab2 - replaced useAsync hook as a separated component

--- lab2/lab2-hooks/src/App.jsx
+++ lab2/lab2-hooks/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useState, useEffect, useCallback } from "react";
+import useAsync from './Hooks/useAsync';
 
 function App() {
   const { execute, status, value, error } = useAsync(fetchUsers, false);
@@ -33,34 +33,4 @@ const fetchUsers = async () => {
   return response.json();
 };
 
-
-const useAsync = (asyncFunc, immediate = true) => {
-  const [status, setStatus] = useState("idle");
-  const [value, setValue] = useState(null);
-  const [error, setError] = useState(null);
-
-  const execute = useCallback(() => {
-    setStatus("pending");
-    setValue(null);
-    setError(null);
-
-    return asyncFunc()
-      .then(response => {
-        setValue(response);
-        setStatus("success");
-      })
-      .catch(error => {
-        setError(error.message);
-        setStatus("error");
-      });
-  }, [asyncFunc]);
-
-  useEffect(() => {
-    if (immediate) {
-      execute();
-    }
-  }, [execute, immediate]);
-
-  return { execute, status, value, error };
-};
 export default App
