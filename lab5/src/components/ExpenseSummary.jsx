279c30ba3a0cb929cc527cf735f03770de086535
Корецький Юрій <ipz221_kyui@student.ztu.edu.ua>
2024-12-20 15:53:16 +0200

    one small correction

--- lab5/src/components/ExpenseSummary.jsx
+++ lab5/src/components/ExpenseSummary.jsx
@@ -8,7 +8,7 @@ const ExpenseSummary = () => {
     const totalFunds = useStore($totalFunds);
     return (
         <Container border={1} sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", mt: 4 }}  >
-          <Typography variant="h5">Total Amount: <span style={{ color: "purple" }}>{totalAmount}&#8372;</span></Typography >
+          <Typography variant="h5">Total Amount: <span style={{ color: "purple" }}>{totalAmount}</span></Typography >
           <Typography variant="h5">Total Funds: <span style={{ color: "green" }}>{totalFunds}&#8372;</span></Typography >
         </Container>
       );
